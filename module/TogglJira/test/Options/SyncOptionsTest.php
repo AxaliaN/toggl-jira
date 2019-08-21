faae836bb3c07f0adacd731c71f52fbf9fd2bfe8
Kevin Kaland <kevin@wizonesolutions.com>
2019-08-21 20:43:54 +0200

    Fix tests.

--- module/TogglJira/test/Options/SyncOptionsTest.php
+++ module/TogglJira/test/Options/SyncOptionsTest.php
@@ -16,18 +16,22 @@ class SyncOptionsTest extends TestCase
         $data = [
             'lastSync' => new \DateTime('2017-04-15T23:35:00+02:00'),
             'jiraUsername' => 'foo',
+            'jiraLoginUsername' => 'foo@example.com',
             'jiraPassword' => 'bar',
             'togglApiKey' => 'foz',
             'jiraUrl' => 'http://www.example.com',
+            'notifyUsers' => true,
         ];
 
         $syncOptions = new SyncOptions($data);
 
         $this->assertEquals($data['lastSync'], $syncOptions->getLastSync());
         $this->assertEquals($data['jiraUsername'], $syncOptions->getJiraUsername());
+        $this->assertEquals($data['jiraLoginUsername'], $syncOptions->getJiraLoginUsername());
         $this->assertEquals($data['jiraPassword'], $syncOptions->getJiraPassword());
         $this->assertEquals($data['togglApiKey'], $syncOptions->getTogglApiKey());
         $this->assertEquals($data['jiraUrl'], $syncOptions->getJiraUrl());
+        $this->assertTrue($syncOptions->isNotifyUsers());
     }
 
     /**
@@ -38,11 +42,13 @@ class SyncOptionsTest extends TestCase
         $data = [
             'lastSync' => new \DateTime('2017-04-15T23:35:00+02:00'),
             'jiraUsername' => 'foo',
+            'jiraLoginUsername' => 'foo@example.com',
             'jiraPassword' => 'bar',
             'togglApiKey' => 'foz',
             'jiraUrl' => 'http://www.example.com',
             'fillIssueID' => 'FOO-01',
             'fillIssueComment' => 'Support',
+            'notifyUsers' => true,
         ];
 
         $syncOptions = new SyncOptions($data);
